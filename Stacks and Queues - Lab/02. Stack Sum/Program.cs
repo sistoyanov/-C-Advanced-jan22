64dccccfb571028fa37987796ba5ac7f501e1676
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-05-16 15:49:29 +0300

    Upload

---
+++ Stacks and Queues - Lab/02. Stack Sum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(input);
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] commandArg = command.Split();
+
+                if(commandArg[0].ToLower() == "add")
+                {
+                    int num1 = int.Parse(commandArg[1]);
+                    int num2 = int.Parse(commandArg[2]);
+                    stack.Push(num1);
+                    stack.Push(num2);
+                }
+                else if(commandArg[0].ToLower() == "remove")
+                {
+                    int count = int.Parse(commandArg[1]);
+
+                    if (count <= stack.Count)
+                    {
+                        for (int i = 1; i <= count ; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
