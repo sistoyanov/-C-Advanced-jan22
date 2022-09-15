f2f95ec495f0da4fe97aca66a8ea01130f5f98a1
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-15 11:28:25 +0300

    Upload

---
+++ Stacks and Queues - Lab/06. Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _06._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> people = new Queue<string>();
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input != "Paid")
+                {
+                    people.Enqueue(input);
+                }
+
+                if (input == "Paid")
+                {
+                    while (people.Count > 0)
+                    {
+                        Console.WriteLine(people.Dequeue());
+                    }
+                }
+                
+            }
+
+            Console.WriteLine($"{people.Count} people remaining.");
+        }
+    }
+}
