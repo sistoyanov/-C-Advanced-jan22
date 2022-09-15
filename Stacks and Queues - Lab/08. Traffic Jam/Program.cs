20a0786b04094c0d554cd5c4a06bc63d1b13c57a
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-15 16:48:41 +0300

    upload

---
+++ Stacks and Queues - Lab/08. Traffic Jam/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _08._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int num = int.Parse(Console.ReadLine());
+          Queue<string> queue = new Queue<string>();
+          int count = 0;
+          string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input != "green")
+                {
+                    queue.Enqueue(input);
+                }
+
+                if (input == "green")
+                {
+                    
+                    for (int i = 0; i < num; i++)
+                    {
+                        if (queue.Count < 1)
+                        {
+                            continue;
+                        }
+
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+               
+            }
+
+            Console.WriteLine($"{count} cars passed the crossroads.");
+     
+        }
+    }
+}
