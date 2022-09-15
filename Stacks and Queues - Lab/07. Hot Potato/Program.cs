fc3649764d71d820d9b106104435d5b962e06944
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-15 11:56:24 +0300

    upload

---
+++ Stacks and Queues - Lab/07. Hot Potato/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _07._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kids = Console.ReadLine().Split();
+            int nums = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>(kids);
+            int tosses = 1;
+
+            while (queue.Count > 1)
+            {
+                string currentKid = queue.Dequeue();
+
+                if (tosses == nums)
+                {
+                    Console.WriteLine($"Removed {currentKid}");
+                    tosses = 1;
+                    continue;
+                }
+
+                tosses++;
+                queue.Enqueue(currentKid);
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
