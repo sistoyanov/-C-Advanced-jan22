ec1e9b059c214aa7178b178f3d6da87b544c63e7
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-15 11:08:52 +0300

    Upload

---
+++ Stacks and Queues - Lab/05. Print Even Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _05._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+            Queue<int> evenNums = new Queue<int>();
+            
+
+            foreach (var item in numbers)
+            {
+                if (item % 2 == 0)
+                {
+                    evenNums.Enqueue(item);
+                }
+            }
+
+            List<int> output = new List<int>();
+
+            while (evenNums.Count > 0)
+            {
+                output.Add(evenNums.Dequeue());
+            }
+
+            Console.WriteLine(string.Join(", ",output));
+
+        }
+    }
+}
