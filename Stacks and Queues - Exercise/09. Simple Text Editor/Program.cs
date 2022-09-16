10ebb8cc40cf809449d947903d13647e818734b1
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-16 17:20:56 +0300

    upload

---
+++ Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _09._Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            StringBuilder text = new StringBuilder();
+            Stack<string> memory = new Stack<string>();
+            memory.Push(String.Empty);
+
+
+            for (int i = 0; i < number; i++)
+            {
+                string[] commandArg = Console.ReadLine().Split().ToArray();
+                string commandType = commandArg[0];
+
+                if (commandType == "1")
+                {
+                    string strToAdd = commandArg[1];
+                    text.Append(strToAdd);
+                    memory.Push(text.ToString());
+                }
+                else if (commandType == "2")
+                {
+                    int elmToRemove = int.Parse(commandArg[1]);
+                    text = text.Remove(text.Length - elmToRemove, elmToRemove);
+                    memory.Push(text.ToString());
+                }
+                else if (commandType == "3")
+                {
+                    int elmToShow = int.Parse(commandArg[1]);
+                    Console.WriteLine(text[elmToShow -1]);
+                }
+                else if (commandType == "4")
+                {
+                    memory.Pop();
+                    string previousVersion = memory.Peek();
+
+                    text = new StringBuilder(previousVersion);
+                }
+            }
+        }
+    }
+}
