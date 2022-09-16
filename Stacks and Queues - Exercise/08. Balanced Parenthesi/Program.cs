11fd87b1828b4af81d34300bc8f3f5dc5f78845a
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-16 16:09:48 +0300

    upload

---
+++ Stacks and Queues - Exercise/08. Balanced Parenthesi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var item in input)
+            {
+                
+                if (stack.Any())
+                {
+                    char check = stack.Peek();
+
+                    if (check == '{' && item == '}')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                    else if (check == '[' && item == ']')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                    else if (check == '(' && item == ')')
+                    {
+                        stack.Pop();
+                       continue;
+
+                    }
+
+                }
+
+                stack.Push(item);
+            }
+            Console.WriteLine(!stack.Any() ? "YES" : "NO");
+        }
+    }
+}
