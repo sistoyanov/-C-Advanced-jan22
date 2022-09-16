554425fa5d4777acdce3f3d1b0acf555585fad6f
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-16 13:32:11 +0300

    upload

---
+++ Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>(clothes);
+            int racks = 1;
+            int sum = capacity;
+
+            while (stack.Any())
+            {
+                int current = stack.Peek();
+
+                if (current <= sum)
+                {
+                    stack.Pop();
+                    sum -= current;
+                }
+                else
+                {
+                    racks++;
+                    sum = capacity;
+                }
+            }
+
+            Console.WriteLine(racks);
+        }
+    }
+}
