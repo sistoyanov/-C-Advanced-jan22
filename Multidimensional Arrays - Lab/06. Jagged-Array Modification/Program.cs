c18232e5745dfc5745730d4e81bf01e6b909d2a5
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-18 15:16:40 +0300

    upload

---
+++ Multidimensional Arrays - Lab/06. Jagged-Array Modification/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[][] jaggedArr = new int[size][];
+
+            for (int i = 0; i < jaggedArr.Length; i++)
+            {
+                int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                jaggedArr[i] = elements;
+            }
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] commandArg = input.Split().ToArray();
+                string commandType = commandArg[0];
+                int row = int.Parse(commandArg[1]);
+                int col = int.Parse(commandArg[2]);
+                int value = int.Parse(commandArg[3]);
+
+                if (row >= 0 && row < jaggedArr.Length && col >= 0 && col < jaggedArr[row].Length)
+                {
+                    if (commandType == "Add")
+                    {
+                        jaggedArr[row][col]+= value;
+                    }
+                    else if (commandType == "Subtract")
+                    {
+                        jaggedArr[row][col] -= value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                } 
+            }
+
+            foreach (var item in jaggedArr)
+            {
+                Console.WriteLine(String.Join(" ", item));
+            }
+        }
+    }
+}
