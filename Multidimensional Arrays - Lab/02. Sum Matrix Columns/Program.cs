bfebdacca965a5abc39a9deead7cec9826b0f3ee
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-18 11:13:21 +0300

    push

---
+++ Multidimensional Arrays - Lab/02. Sum Matrix Columns/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] elements = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                int sum = 0;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
