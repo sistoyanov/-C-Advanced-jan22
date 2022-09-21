2bc046d1f695ccb7d22ef57467b3792e2c5af3b5
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-21 15:41:32 +0300

    Yep

---
+++ Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+            }
+
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
