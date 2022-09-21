facdbc451af805244880b204544b00bf6470cf57
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-21 14:06:37 +0300

    upload

---
+++ Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            Dictionary<double, int> dictionary = new Dictionary<double, int>();
+
+            foreach (var item in input)
+            {
+                if (!dictionary.ContainsKey(item))
+                {
+                    dictionary.Add(item, 0);
+                }
+
+                dictionary[item]++;
+            }
+
+            foreach (var kvp in dictionary)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
