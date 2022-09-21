697ddbaa9f3ace4fa0cac723de14cf8b8f77f2d1
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-21 14:44:34 +0300

    upload

---
+++ Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().
+                            Split(" ", StringSplitOptions.RemoveEmptyEntries).
+                            Select(int.Parse).
+                            OrderByDescending(n => n).
+                            Take(3).
+                            ToArray();
+
+            Console.WriteLine($"{String.Join(" ", numbers)}");
+        }
+    }
+}
