08e5cde311c0e3b037f001ce2357f36da2f33c8a
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-22 18:26:07 +0300

    Upload

---
+++ Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] symbols = Console.ReadLine().ToCharArray();
+            SortedDictionary<char, int> dictionary = new SortedDictionary<char, int>();
+            
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                char currentChar = symbols[i];
+
+                if (!dictionary.ContainsKey(currentChar))
+                {
+                    dictionary.Add(currentChar, 0);
+                }
+
+                dictionary[currentChar]++;
+            }
+
+            foreach (var kvp in dictionary)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
