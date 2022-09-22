47ac00e5348a650599e52b82770e547003ec4f4d
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-22 17:33:03 +0300

    Upload

---
+++ Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            HashSet<string> guestList = new HashSet<string>();
+
+
+            while ((input = Console.ReadLine()) != "PARTY")
+            {
+                guestList.Add(input);
+            }
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                guestList.Remove(input);
+            }
+
+            Console.WriteLine(guestList.Count);
+
+            //foreach (var guest in guestList)
+            //{
+            //    if (guest.Any(char.IsDigit))
+            //    {
+            //        Console.WriteLine(guest);
+            //    }
+
+            //    guestList.Remove(guest);
+            //}
+
+            //foreach (var guest in guestList)
+            //{
+            //    if (guest.Any(char.IsLetter))
+            //    {
+            //        Console.WriteLine(guest);
+            //    }
+            //}
+
+            foreach (var item in guestList)
+            {
+                char[] ch = item.ToCharArray();
+                if (char.IsDigit(ch[0]))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            foreach (var item in guestList)
+            {
+                char[] ch = item.ToCharArray();
+                if (char.IsLetter(ch[0]))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+    }
+}
