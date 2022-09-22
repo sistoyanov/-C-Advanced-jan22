d7318e2c6d871b08bc0004289c67d4b1d7e469fc
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-22 17:02:40 +0300

    Upld

---
+++ Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            HashSet<string> set = new HashSet<string>();
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] commandArg = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string commandType = commandArg[0];
+                string carLicense = commandArg[1];
+
+                if (commandType == "IN")
+                {
+                    set.Add(carLicense);
+                }
+                else if (commandType == "OUT")
+                {
+                    set.Remove(carLicense);
+                }
+            }
+
+            if (set.Count > 0)
+            {
+                foreach (var license in set)
+                {
+                    Console.WriteLine(license);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
