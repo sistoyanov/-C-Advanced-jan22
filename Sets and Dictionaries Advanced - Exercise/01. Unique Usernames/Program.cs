b8cbe73eccb451e7a73c1813a044effc865814a0
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-22 17:46:30 +0300

    Upload

---
+++ Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                set.Add(input);
+            }
+
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
