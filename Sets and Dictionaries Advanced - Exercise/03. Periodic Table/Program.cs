6e67bd9eb3251de24b59da2ebcc92c0406c095dd
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-22 18:06:59 +0300

    Upload

---
+++ Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            SortedSet<string> set = new SortedSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    set.Add(input[j]);
+                }
+                
+            }
+
+            Console.WriteLine(String.Join(" ", set));
+
+        }
+    }
+}
