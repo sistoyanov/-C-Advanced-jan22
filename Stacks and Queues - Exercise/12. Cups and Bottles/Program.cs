b0ce8e093ed7433b4678720703a6837a50c35549
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-23 12:30:35 +0300

    Upload

---
+++ Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Cups_and_Bottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cupsDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] bottlesDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Queue<int> cups =new Queue<int>(cupsDetails);
+            Stack<int> bottles = new Stack<int>(bottlesDetails);
+
+            int wastedWatter = 0;
+
+            while (cups.Count > 0 && bottles.Count > 0)
+            {
+                int currentCup = cups.Dequeue();
+
+                while (currentCup > 0)
+                {
+                    int currentBottle = bottles.Pop();
+                    currentCup -= currentBottle;
+
+                    if (currentCup < 0)
+                    {
+                        wastedWatter += Math.Abs(currentCup);
+                    }
+
+                }
+            }
+
+            if (bottles.Count > 0)
+            {
+                Console.Write("Bottles: ");
+                Console.WriteLine(String.Join(" ", bottles));
+            }
+            else if (cups.Count > 0)
+            {
+                Console.Write("Cups: ");
+                Console.WriteLine(String.Join(" ", cups));
+            }
+
+            Console.WriteLine($"Wasted litters of water: {wastedWatter}");
+        }
+    }
+}
