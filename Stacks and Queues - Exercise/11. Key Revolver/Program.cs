53ca43d5dc85b26687f5e5d388801514a1d4a5d4
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-23 11:39:00 +0300

    push

---
+++ Stacks and Queues - Exercise/11. Key Revolver/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._Key_Revolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int barrelSize = int.Parse(Console.ReadLine());
+            int[] bulletsDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] locksDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int reward = int.Parse(Console.ReadLine());
+
+            Stack<int> bullets = new Stack<int>(bulletsDetails);
+            Queue<int> locks = new Queue<int>(locksDetails);
+
+            int bulletsShot = 0;
+
+            while (locks.Count > 0 && bullets.Count > 0)
+            {
+                int currentLock = locks.Peek();
+                int currentBullet = bullets.Pop();
+                bulletsShot++;
+
+                if (currentBullet <= currentLock)
+                {
+                    locks.Dequeue();
+                    Console.WriteLine("Bang!");
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                if (bulletsShot % barrelSize == 0 && bullets.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                }
+            }
+
+            if (bullets.Count <= 0 && locks.Count > 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+            else
+            {
+                int moneyEarned = reward - (bulletPrice * bulletsShot);
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${moneyEarned}");
+            }
+        }
+    }
+}
