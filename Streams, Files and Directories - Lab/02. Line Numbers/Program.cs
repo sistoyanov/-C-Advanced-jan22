415f79b8189ae0026528c2c397cda987e7bc2b3f
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-24 15:02:18 +0300

    Upload

---
+++ Streams, Files and Directories - Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            StreamWriter writer = new StreamWriter(outputFilePath);
+
+            using (reader)
+            {
+                int counter = 0;
+                string line = String.Empty;
+
+                using (writer)
+                {
+                  while ((line = reader.ReadLine()) != null)
+                  {
+                    counter++;
+                    writer.WriteLine($"{counter}. {line}");
+                  
+                  }
+
+                }
+
+            }
+        }
+    }
+}
