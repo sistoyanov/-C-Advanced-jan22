55525f41adf04b3f6cb660bcb969723e116cb5ed
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-26 12:13:45 +0300

    Upload

--- Streams, Files and Directories - Lab/03. Word Count/Program.cs
+++ Streams, Files and Directories - Lab/03. Word Count/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace WordCount
 {
@@ -15,7 +19,42 @@ namespace WordCount
 
         public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
         {
-            // TODO: write your code here…
+            string[] words = File.ReadAllText(wordsFilePath).Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string[] lines = File.ReadAllLines(textFilePath);
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!wordCounts.ContainsKey(word))
+                {
+                    wordCounts.Add(word, 0);
+                }
+            }
+
+            foreach (var line in lines)
+            {
+                string regex = @"[\w\']+";
+                MatchCollection matches = Regex.Matches(line, regex);
+
+                foreach (Match match in matches)
+                {
+                   string currentWord = match.Value.ToLower();
+                    
+                    if (wordCounts.ContainsKey(currentWord))
+                    {
+                        wordCounts[currentWord]++;
+                    }
+                }
+            }
+
+            wordCounts = wordCounts.OrderByDescending(v => v.Value).ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var kvp in wordCounts)
+            {
+
+                string output = $"{kvp.Key} - {kvp.Value}";
+                File.AppendAllText(outputFilePath, output + Environment.NewLine);
+            }
         }
     }
 }
