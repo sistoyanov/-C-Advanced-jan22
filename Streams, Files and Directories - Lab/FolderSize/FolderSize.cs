be195eca11fbce11306764ace0acb11735563ac1
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-27 12:27:22 +0300

    Upload

--- Streams, Files and Directories - Lab/FolderSize/FolderSize.cs
+++ Streams, Files and Directories - Lab/FolderSize/FolderSize.cs
@@ -14,6 +14,29 @@
 
         public static void GetFolderSize(string folderPath, string outputFilePath)
         {
+            DirectoryInfo folderInfo = new DirectoryInfo(folderPath);
+            DirectoryInfo[] subFolders = folderInfo.GetDirectories();
+            long totalSize = 0;
+
+            FileInfo[] allFiles = folderInfo.GetFiles();
+
+            foreach (var file in allFiles)
+            {
+                totalSize += file.Length;
+            }
+
+            foreach (var dir in subFolders)
+            {
+                FileInfo[] allSbFiles = dir.GetFiles();
+
+                foreach (var sbFile in allSbFiles)
+                {
+                    totalSize += sbFile.Length;
+                }
+            }
+
+            double newSize = totalSize / 1024d;
+            File.WriteAllText(outputFilePath, $"{newSize} KB");
         }
     }
 }
