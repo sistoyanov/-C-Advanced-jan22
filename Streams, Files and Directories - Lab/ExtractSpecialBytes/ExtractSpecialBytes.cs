10a1adc5a1e03340bc3968d725b577af2745244a
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-27 16:19:27 +0300

    Upload

--- Streams, Files and Directories - Lab/ExtractSpecialBytes/ExtractSpecialBytes.cs
+++ Streams, Files and Directories - Lab/ExtractSpecialBytes/ExtractSpecialBytes.cs
@@ -1,8 +1,8 @@
-﻿namespace ExtractSpecialBytes
+﻿namespace ExtractBytes
 {
     using System;
     using System.IO;
-    public class ExtractSpecialBytes
+    public class ExtractSBytes
     {
         static void Main()
         {
@@ -15,6 +15,35 @@
 
         public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
         {
+            FileStream image = new FileStream(binaryFilePath, FileMode.Open, FileAccess.Read);
+            FileStream bytes = new FileStream(bytesFilePath, FileMode.Open, FileAccess.Read);
+            FileStream output = new FileStream(outputPath, FileMode.Create, FileAccess.Write);
+
+            byte[] imageBuffer = new byte[image.Length];
+            byte[] bytesBuffer = new byte[bytes.Length];
+
+            using (image)
+            {
+                using (bytes)
+                {
+                    image.Read(imageBuffer, 0, (int)image.Length);
+                    bytes.Read(bytesBuffer, 0, (int)bytes.Length);
+                }
+            }
+
+            using (output)
+            {
+                for (int i = 0; i < imageBuffer.Length; i++)
+                {
+                    for (int j = 0; j < bytesBuffer.Length; j++)
+                    {
+                        if (imageBuffer[i] == bytesBuffer[j])
+                        {
+                            output.Write(new byte[] { imageBuffer[i] });
+                        }
+                    }
+                }
+            }
         }
     }
 }
