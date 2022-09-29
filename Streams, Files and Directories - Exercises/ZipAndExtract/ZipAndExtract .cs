ae5987ea0813ab419cc7680bafdfe514ccfc8a10
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-29 17:19:41 +0300

    Upload

--- Streams, Files and Directories - Exercises/ZipAndExtract/ZipAndExtract .cs
+++ Streams, Files and Directories - Exercises/ZipAndExtract/ZipAndExtract .cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.IO;
+    using System.IO.Compression;
+
     public class ZipAndExtract
     {
         static void Main()
@@ -18,12 +20,26 @@
 
         public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
         {
-            throw new NotImplementedException();
+            ZipArchive archive = null;
+
+            using (archive)
+            {
+                archive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create);
+                string fileName = Path.GetFileName(inputFilePath);
+                archive.CreateEntryFromFile(inputFilePath, fileName);
+            }
         }
 
         public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
         {
-            throw new NotImplementedException();
+            ZipArchive archive = null;
+
+            using (archive)
+            {
+                archive = ZipFile.OpenRead(zipArchiveFilePath);
+                ZipArchiveEntry fileForExtract = archive.GetEntry(fileName);
+                fileForExtract.ExtractToFile(outputFilePath);
+            }
         }
     }
 }
