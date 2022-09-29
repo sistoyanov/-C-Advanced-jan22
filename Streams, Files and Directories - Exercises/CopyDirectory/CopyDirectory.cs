c5d10f36f8e21f48f9f523bafdea87c83e43db82
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-29 16:07:21 +0300

    Upload

--- Streams, Files and Directories - Exercises/CopyDirectory/CopyDirectory.cs
+++ Streams, Files and Directories - Exercises/CopyDirectory/CopyDirectory.cs
@@ -1,6 +1,8 @@
 ﻿namespace CopyDirectory
 {
     using System;
+    using System.IO;
+
     public class CopyDirectory
     {
         static void Main()
@@ -13,7 +15,21 @@
 
         public static void CopyAllFiles(string inputPath, string outputPath)
         {
-            throw new NotImplementedException();
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath);
+            }
+
+            Directory.CreateDirectory(outputPath);
+
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach (var file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                string destination = Path.Combine(outputPath, fileName);
+                File.Copy(file, destination);
+            }
         }
     }
 }
