818d138dbbde47640d400343587cd2eaf6be48a2
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-30 11:00:02 +0300

    Upload

---
+++ Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = (names) => Console.WriteLine(String.Join(Environment.NewLine, names).ToString());
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            print(input);
+        }
+    }
+}
