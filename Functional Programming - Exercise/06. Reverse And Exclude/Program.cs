afbcbe2d36506ccd431dddf55ef33fc91a2b85b3
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-30 14:55:16 +0300

    Upload

---
+++ Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int, int[]> removeDevisible = (numbers, divider) => numbers.Where(x => x % divider != 0).ToArray();
+
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int devider = int.Parse(Console.ReadLine());
+            numbers = removeDevisible(numbers, devider).Reverse().ToArray();
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
