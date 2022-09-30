d47905b98811c3134d862a20c1d8a8e506cdd441
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-30 15:05:53 +0300

    Upload

---
+++ Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string[], int, string[]> filterByLength = (names, length) => names.Where(n => n.Length <= length).ToArray();
+
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(String.Join(Environment.NewLine, filterByLength(names, length)));
+        }
+    }
+}
