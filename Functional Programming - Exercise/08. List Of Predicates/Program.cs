0e7bc69a28a343878ec7c745f38a0d640b889c55
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-30 15:32:06 +0300

    Upoad

---
+++ Functional Programming - Exercise/08. List Of Predicates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+            Func<int[], List<Predicate<int>>, int[]> removedDevisible = (numbers, predicates) =>
+            {
+                foreach (var predicate in predicates)
+                {
+                    numbers = Array.FindAll(numbers, predicate).ToArray();
+                }
+
+                return numbers;
+            };
+
+            int count = int.Parse(Console.ReadLine());
+            int[] numbers = Enumerable.Range(1, count).ToArray();
+            HashSet<int> inputDividers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
+
+            foreach (var divider in inputDividers)
+            {
+                predicates.Add(p => p % divider == 0);
+            }
+
+            Console.WriteLine(String.Join(" ", removedDevisible(numbers, predicates)));
+        }
+    }
+}
