f36a1201f1142550422367b95cf0898eb9cd866d
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-30 11:05:09 +0300

    Upload

---
+++ Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = (names) =>
+            {
+                StringBuilder output = new StringBuilder();
+                foreach (var name in names)
+                {
+                    output.AppendLine($"Sir {name}");
+                }
+
+                Console.WriteLine(output.ToString());
+            };
+            
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            print(input);
+        }
+    }
+}
