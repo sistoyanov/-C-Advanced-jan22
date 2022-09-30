97e6b075e881a514e9b5ec46874ec42f58d9b602
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-09-30 17:29:44 +0300

    Upload

---
+++ Functional Programming - Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> checkLength = (name, length) => name.Sum(ch => ch) >= length;
+            Func<string[], int, Func<string, int, bool>, string> getFirst = (names, lenght, checkLength) => names.FirstOrDefault(name => checkLength(name, lenght));
+            
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(getFirst(names, length, checkLength));
+        }
+    }
+}
