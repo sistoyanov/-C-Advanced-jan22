bb51a300f55d95e8a09b283218293a99a677b691
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-02 19:20:22 +0300

    Upload

---
+++ Defining Classes - Lab/CarManufacturer/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Schema;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+
+        public Car(string make, string model, int year, int horsePower, double fuelQuantity,
+            double fuelConsumption, int engineIndex, int tiresIndex, double totalPressure)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            HorsePower = horsePower;
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+            EngineIndex = engineIndex;
+            TiresIndex = tiresIndex;
+            TotalPressure = totalPressure;
+        }
+
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public int EngineIndex { get; set; }
+
+        public int TiresIndex { get; set; }
+
+        public double TotalPressure { get; set; }
+
+
+        public double Drive(double fuelQuantity, double fuelConsumption, int distance)
+        {
+            fuelQuantity -= (FuelConsumption / 100) * distance;
+
+            return fuelQuantity;
+        }
+
+    }
+}
---
+++ Defining Classes - Lab/CarManufacturer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            List<List<double>> listTiresYears = new List<List<double>>();
+            List<List<double>> listTiresPressures = new List<List<double>>();
+            List<int> listHorsePowers = new List<int>();
+            List<double> listCubicCapacity = new List<double>();
+
+            List<Car> listCars = new List<Car>();
+
+            string input = String.Empty;
+
+            Tire tires = new Tire();
+            Engine engine = new Engine();
+
+            while ((input = Console.ReadLine()) != "No more tires")
+            {
+                string[] splitted = input.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                List<double> listYears = tires.GetYearInfo(splitted);
+                List<double> listPressures = tires.GetPressureInfo(splitted);
+
+                listTiresYears.Add(listYears);
+                listTiresPressures.Add(listPressures);
+            }
+
+            while ((input = Console.ReadLine()) != "Engines done")
+            {
+                string[] splitted = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                listHorsePowers.Add(engine.GetHorsePower(splitted));
+                listCubicCapacity.Add(engine.GetCubicCapacity(splitted));
+            }
+
+            while ((input = Console.ReadLine()) != "Show special")
+            {
+                string[] splitted = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string make = splitted[0];
+                string model = splitted[1];
+                int year = int.Parse(splitted[2]);
+                double fuelQuantity = double.Parse(splitted[3]);
+                double fuelConsumption = double.Parse(splitted[4]);
+                int engineIndex = int.Parse(splitted[5]);
+                int tiresIndex = int.Parse(splitted[6]);
+
+                int horsePower = listHorsePowers[engineIndex];
+                double pressure = tires.GetSumPressure(listTiresPressures, tiresIndex);
+
+                Car car = new Car(make, model, year, horsePower, fuelQuantity, fuelConsumption,
+                    engineIndex, tiresIndex, pressure);
+
+                listCars.Add(car);
+            }
+
+            foreach (var car in listCars)
+            {
+                if (car.Year >= 2017 && car.HorsePower > 330 && car.TotalPressure > 9 && car.TotalPressure < 10)
+                {
+                    car.FuelQuantity = car.Drive(car.FuelQuantity, car.FuelConsumption, 20);
+
+                    Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}\nHorsePowers: {car.HorsePower}\nFuelQuantity: {car.FuelQuantity}");
+                };
+
+            }
+        }
+    }
+}
