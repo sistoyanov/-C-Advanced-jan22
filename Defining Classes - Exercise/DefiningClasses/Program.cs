956b46b1ad2596e4678ec51a5b8d318faaef24ff
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-03 15:30:06 +0300

    Upload

---
+++ Defining Classes - Exercise/DefiningClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personList = new List<Person>();
+            string[] personDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string personName = personDetails[0];
+            int personAge = int.Parse(personDetails[1]);
+            Person personToAdd = new Person(personName, personAge);
+            personList.Add(personToAdd);
+        }
+    }
+}
