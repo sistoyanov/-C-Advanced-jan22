62e904e83cb8acceaf93ec0656b5bb9861edde09
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-03 17:03:31 +0300

    Upload

---
+++ Defining Classes - Exercise/DefiningClasses/Family.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            this.PersonsList = new List<Person>();
+        }
+
+        private List<Person> presonsList;
+
+        public List<Person> PersonsList
+        {
+            get { return presonsList; }
+            set { presonsList = value; }
+        }
+
+
+        public void AddMember(Person memmber)
+        {
+            presonsList.Add(memmber);
+        }
+
+        public List<Person> GetOldestMember()
+        {
+            int oldestValue = 30; //PersonsList.Max(p => p.Age);
+            List<Person> oldestPersons = PersonsList.FindAll(x => x.Age > oldestValue);
+            
+           return oldestPersons;
+        }
+
+    }
+}
--- Defining Classes - Exercise/DefiningClasses/Program.cs
+++ Defining Classes - Exercise/DefiningClasses/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace DefiningClasses
 {
@@ -7,12 +9,28 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            List<Person> personList = new List<Person>();
-            string[] personDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            string personName = personDetails[0];
-            int personAge = int.Parse(personDetails[1]);
-            Person personToAdd = new Person(personName, personAge);
-            personList.Add(personToAdd);
+            int counter = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < counter; i++)
+            {
+                string[] personDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string personName = personDetails[0];
+                int personAge = int.Parse(personDetails[1]);
+
+                Person personToAdd = new Person(personName, personAge);
+                family.AddMember(personToAdd);
+            }
+
+            List<Person> olderThan30 = family.GetOldestMember().OrderBy(x => x.Name).ToList();
+            foreach (var person in olderThan30)
+            {
+                output.AppendLine($"{person.Name} - {person.Age}");
+            }
+
+            Console.WriteLine(output.ToString());
         }
     }
 }
