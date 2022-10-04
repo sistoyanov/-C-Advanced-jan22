b7ac057cb20369199f7bc5f41430fe04403cacd6
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-04 14:33:57 +0300

    Upload

---
+++ Defining Classes - Exercise/DateModifier/DateModifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.XPath;
+
+namespace DateModifier
+{
+    public static class DateModifier
+    {
+        public static string StarDate { get; set; }
+        public static string EndDate { get; set; }
+
+        public static int CalculateDifference(string startDateString, string endDateStirng)
+        {
+            DateTime startDate = DateTime.Parse(startDateString);
+            DateTime endDate = DateTime.Parse(endDateStirng);
+
+            TimeSpan timeSpan = endDate - startDate;
+      
+            return Math.Abs(timeSpan.Days);
+        }
+    }
+}
