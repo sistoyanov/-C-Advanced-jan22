9432b7862c6a2426aa92d2f21ffa3f3a1b92ae08
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-05 11:59:39 +0300

    Upload

---
+++ Defining Classes - Exercise/PokemonTrainer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            List<Trainer> trainers = new List<Trainer>();
+
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                string[] inputDetails = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string trainerName = inputDetails[0];
+                string pokemonName = inputDetails[1];
+                string pokemonElement = inputDetails[2];
+                int pokemonHealth = int.Parse(inputDetails[3]);
+
+                Pokemon pokemonToAdd = new Pokemon(pokemonName,pokemonElement,pokemonHealth);
+
+                if (!trainers.Any(t => t.Name == trainerName))
+                {
+                    Trainer newTrainer = new Trainer(trainerName);
+                    trainers.Add(newTrainer);
+                    newTrainer.AddPokemon(pokemonToAdd);
+                }
+                else
+                {
+                    Trainer currentTrainer = trainers.FirstOrDefault(t => t.Name == trainerName);
+                    currentTrainer.AddPokemon(pokemonToAdd);
+                }
+            }
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string currentCommand = input;
+                CheckPokemons(currentCommand, trainers);
+            }
+
+            trainers = trainers.OrderByDescending(t => t.Bages).ToList();
+
+            foreach (var trainer in trainers)
+            {
+                Console.WriteLine(trainer);
+            }
+        }
+
+        private static void CheckPokemons(string currentComand, List<Trainer> trainers)
+        {
+            foreach (var trainer in trainers)
+            {
+                if (trainer.Pokemons.Any(p => p.Element == currentComand))
+                {
+                    trainer.Bages++;
+                }
+                else
+                {
+                    trainer.Pokemons.ForEach(p => p.Health -= 10);
+                    trainer.Pokemons.RemoveAll(p => p.Health <= 0);
+                }
+
+            }
+        }
+    }
+}
---
+++ Defining Classes - Exercise/PokemonTrainer/Trainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            this.Name = name;
+            this.Bages = 0;
+            this.Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public int Bages { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+        public void AddPokemon(Pokemon pokemon)
+        {
+            Pokemons.Add(pokemon);
+        }
+
+        public override string ToString()
+        { 
+            return $"{this.Name} {this.Bages} {this.Pokemons.Count}";
+        }
+    }
+}
