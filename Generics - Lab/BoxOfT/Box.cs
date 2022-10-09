489d54cdb1a2aa0d2ff5edfb565b5abe5b5016ac
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-09 12:33:24 +0300

    Upload

---
+++ Generics - Lab/BoxOfT/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> stack = new Stack<T>();
+        public int Count { get { return stack.Count; } }
+
+        public void Add(T element)
+        {
+            stack.Push(element);
+        }
+
+        public T Remove()
+        {
+            return stack.Pop();
+        }
+    }
+}
