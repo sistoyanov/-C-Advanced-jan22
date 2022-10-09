0a11e5759e793285d3afa254ea37557b54c6f184
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-09 13:12:36 +0300

    Up

---
+++ Generics - Lab/GenericScale/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> numbers = new EqualityScale<int>(6, 5);
+            Console.WriteLine(numbers.AreEqual());
+
+            EqualityScale<string> words = new EqualityScale<string>("yes", "yes");
+            Console.WriteLine(words.AreEqual());
+        }
+    }
+}
