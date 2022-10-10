471ef393c981bef840a8534894ec131b9182d1d7
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-10 11:32:50 +0300

    up

---
+++ Generics - Exercise/CountMethodStrings/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountMethodStrings
+{
+    public class Box<T> where T : IComparable
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+        public T Value { get; set; }
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+        
+    }
+}
---
+++ Generics - Exercise/CountMethodStrings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Box<string>> boxList = new List<Box<string>>();
+
+            for (int i = 0; i < count; i++)
+            {
+                Box<string> newBox = new Box<string>(Console.ReadLine());
+                boxList.Add(newBox);
+            }
+
+            string comperer = Console.ReadLine();
+            Console.WriteLine(CountComparable(boxList, comperer));
+        }
+
+        public static int CountComparable<T>(List<T> boxList, string comperer)
+        {
+            int counter = 0;
+
+            foreach (T box in boxList)
+            {
+                int result = string.Compare(box.ToString(), comperer);
+
+                if (result > 0)
+                {
+                    counter++;
+                }
+
+            }
+
+            return counter;
+        }
+    }
+}
