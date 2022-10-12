c7f407eb12574ebc04ee5fa4f31ae8f227873c5f
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-12 10:08:58 +0300

    Upload

---
+++ Iterators and Comparators - Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIdex;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current => this.books[this.currentIdex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose(){}
+
+            public bool MoveNext()
+            {
+                return ++this.currentIdex < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                this.currentIdex = -1;
+            }
+        }
+    }
+}
