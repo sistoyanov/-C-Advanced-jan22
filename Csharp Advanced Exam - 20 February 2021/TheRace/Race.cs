7a7a7c7cbc6b65070033788cdc6a64a859230c49
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-13 17:41:59 +0300

    Upload

---
+++ Csharp Advanced Exam - 20 February 2021/TheRace/Race.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    public class Race
+    {
+        private List<Racer> racers;
+        public Race(string name, int capacity)
+        {
+            this.racers = new List<Racer>();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return racers.Count; } }
+
+        public void Add(Racer racer)
+        {
+            if (Count < Capacity)
+            {
+                racers.Add(racer);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Racer racerToRemove = racers.FirstOrDefault(r => r.Name == name);
+
+            if (racerToRemove != null)
+            {
+                racers.Remove(racerToRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Racer GetOldestRacer()
+        {
+            return racers.OrderByDescending(r => r.Age).FirstOrDefault();
+        }
+
+        public Racer GetRacer(string name)
+        {
+            return racers.FirstOrDefault(r => r.Name == name);
+        }
+
+        public Racer GetFastestRacer()
+        {
+            return racers.OrderByDescending(r => r.Car.Speed).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder output = new StringBuilder();
+
+            output.AppendLine($"Racers participating at {Name}:");
+
+            foreach (var racer in racers)
+            {
+                output.AppendLine(racer.ToString());
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
