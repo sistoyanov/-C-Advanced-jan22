eec8a2ad442a2d431db458d8d544fbc890f40947
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-14 14:42:56 +0300

    Up

---
+++ Csharp Advanced Retake Exam - 14 April 2021/01.Warm Winter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Warm_Winter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] hatsDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] scarfsDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Stack<int> hats = new Stack<int>(hatsDetails);
+            Queue<int> scarfs = new Queue<int>(scarfsDetails);
+            List<int> sets = new List<int>();
+
+            while (hats.Count >0 && scarfs.Count > 0)
+            {
+                int currentHat = hats.Peek();
+                int currentScarf = scarfs.Peek();
+
+                if (currentHat > currentScarf)
+                {
+                    sets.Add(currentHat + currentScarf);
+                    hats.Pop();
+                    scarfs.Dequeue();
+                }
+                else if (currentHat < currentScarf)
+                {
+                    hats.Pop();
+                }
+                else
+                {
+                    scarfs.Dequeue();
+                    hats.Push(hats.Pop() + 1);
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {sets.Max()}");
+            Console.WriteLine(String.Join(" ", sets));
+        }
+    }
+}
