eb357ce98b4f501c3fd0497040dbbfbb5636cdcb
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-14 18:12:09 +0300

    Upload

---
+++ Iterators and Comparators - Exercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> items;
+        private int index;
+
+        public ListyIterator(List<T> list)
+        {
+            this.items = new List<T>(list);
+        }
+
+        public bool Move()
+        {
+            if (index < items.Count - 1)
+            {
+                index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool	HasNext()
+        {
+            if (index < items.Count - 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            if (items.Count > 0)
+            {
+                Console.WriteLine(items[index]);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/ListyIterator/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string items = Console.ReadLine();
+            ListyIterator<string> collection = new ListyIterator<string>(items.Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).ToList());
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string commad = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).First();
+                
+                if (commad == "Move")
+                {
+                    Console.WriteLine(collection.Move());
+                }
+                else if (commad == "Print")
+                {
+                    
+                    try
+                    {
+                        collection.Print();
+                    }
+                    catch (Exception exception)
+                    {
+
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+                else if (commad == "HasNext")
+                {
+                    Console.WriteLine(collection.HasNext());
+                }
+            }
+        }
+    }
+}
