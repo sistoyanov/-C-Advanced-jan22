3f0602b449f9109e067c9625cf8c41ff4ff3a17a
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-16 17:42:19 +0300

    Upload

---
+++ Csharp Advanced Exam - 26 June 2021/SkiRental/SkiRental.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkiRental
+{
+    public class SkiRental
+    {
+
+        private List<Ski> skis;
+        public SkiRental(string name, int capacity)
+        {
+            skis = new List<Ski>();
+            Name = name;
+            Capacity = capacity;
+        }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return skis.Count; } }
+
+        public void Add(Ski ski)
+        {
+            if (Count < Capacity)
+            {
+                skis.Add(ski);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            return skis.Remove(skis.FirstOrDefault(s => s.Manufacturer == manufacturer && s.Model == model));
+        }
+
+        public Ski GetNewestSki()
+        {
+            return skis.OrderByDescending(s => s.Year).FirstOrDefault();
+        }
+
+        public Ski GetSki(string manufacturer, string model)
+        {
+            return skis.FirstOrDefault(s => s.Manufacturer == manufacturer && s.Model == model);
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder ouput = new StringBuilder();
+            ouput.AppendLine($"The skis stored in {Name}:");
+            foreach (Ski ski in skis)
+            {
+                ouput.AppendLine(ski.ToString());
+            }
+            return ouput.ToString().TrimEnd();
+        }
+    }
+}
