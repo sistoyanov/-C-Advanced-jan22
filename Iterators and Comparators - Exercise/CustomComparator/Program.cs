c9297acab6fac72e1b0d69de5a98a64853a23e6b
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-18 11:33:11 +0300

    Up

---
+++ Iterators and Comparators - Exercise/CustomComparator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    public class StarUp
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> customComperator = (a, b) =>
+            {
+                if (a % 2 == 0 && b % 2 != 0)
+                {
+                    return -1;
+                }
+
+                if (a % 2 != 0 && b % 2 == 0)
+                {
+                    return 1;
+                }
+
+                return a.CompareTo(b);
+            };
+
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Array.Sort(numbers, (a, b) => customComperator(a, b));
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
