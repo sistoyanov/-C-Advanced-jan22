0dd744e20f7de2fbadc8548a99c61e88090bc9f2
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-18 17:30:11 +0300

    Upload

---
+++ Csharp Advanced Exam - 23 October 2021/StockMarket/Investor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        private List<Stock> portfolio;
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            portfolio = new List<Stock>();
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+        }
+
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+        public int Count => portfolio.Count;
+
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10_000 && MoneyToInvest >= stock.PricePerShare)
+            {
+                MoneyToInvest -= stock.PricePerShare;
+                portfolio.Add(stock);
+            }
+        }
+
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            Stock stockToSell = portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+
+            if (stockToSell == null)
+            {
+                return $"{companyName} does not exist.";
+            }
+
+            if (stockToSell.PricePerShare > sellPrice)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+
+            MoneyToInvest += sellPrice;
+            portfolio.Remove(stockToSell);
+            return $"{companyName} was sold.";
+        }
+
+        public Stock FindStock(string companyName) => portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+
+        public Stock FindBiggestCompany() => portfolio.OrderByDescending(s => s.MarketCapitalization).FirstOrDefault();
+
+        public string InvestorInformation()
+        {
+            StringBuilder output = new StringBuilder();
+            
+            output.AppendLine($"The investor {FullName} with a broker {BrokerName} has stocks:");
+            foreach (var stock in portfolio)
+            {
+                output.AppendLine(stock.ToString());
+            }
+
+            return output.ToString().TrimEnd();
+        }
+
+
+    }
+}
---
+++ Csharp Advanced Exam - 23 October 2021/StockMarket/Stock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StockMarket
+{
+    public class Stock
+    {
+        public Stock(string companyName, string director, decimal pricePerShare, int totalNumberOfShares)
+        {
+            CompanyName = companyName;
+            Director = director;
+            PricePerShare = pricePerShare;
+            TotalNumberOfShares = totalNumberOfShares;
+            MarketCapitalization = totalNumberOfShares * pricePerShare;
+        }
+
+        public string CompanyName { get; set; }
+        public string Director { get; set; }
+        public decimal PricePerShare { get; set; }
+        public int TotalNumberOfShares { get; set; }
+        public decimal MarketCapitalization { get; set; }
+
+        public override string ToString()
+        {
+            return $"Company: {CompanyName}{Environment.NewLine}" +
+                   $"Director: {Director}{Environment.NewLine}" +
+                   $"Price per share: ${PricePerShare}{Environment.NewLine}" +
+                   $"Market capitalization: ${MarketCapitalization}";
+                    
+
+        }
+    }
+}
