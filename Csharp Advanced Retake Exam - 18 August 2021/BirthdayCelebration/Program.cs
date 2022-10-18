70163e75560cfbff64820ac7404c1470cfabc400
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-18 14:04:01 +0300

    Up

---
+++ Csharp Advanced Retake Exam - 18 August 2021/BirthdayCelebration/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BirthdayCelebration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] guestDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] platesDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            List<int> guests = new List<int>(guestDetails);
+            Stack<int> plates = new Stack<int>(platesDetails);
+            int wasteFood = 0;
+
+            while (guests.Count > 0 && plates.Count > 0)
+            {
+                int currentPlate = plates.Pop();
+                guests[0] -= currentPlate;
+
+                if (guests[0] <= 0)
+                {
+                    wasteFood += Math.Abs(guests[0]);
+                    guests.RemoveAt(0);
+                    
+                }
+            }
+
+            if (plates.Count > 0)
+            {
+                Console.WriteLine($"Plates: {String.Join(" ", plates)}");
+            }
+            else if (guests.Count > 0)
+            {
+                Console.WriteLine($"Guests: {String.Join(" ", guests)}");
+            }
+
+            Console.WriteLine($"Wasted grams of food: {wasteFood}");
+        }
+    }
+}
