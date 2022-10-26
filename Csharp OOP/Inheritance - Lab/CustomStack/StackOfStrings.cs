02809747ee763b20dd1cb3234341da60aec92b9c
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-26 10:30:03 +0300

    Upload

---
+++ Csharp OOP/Inheritance - Lab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => this.Count == 0;
+
+        public Stack<string> AddRange(params string[] range)
+        {
+            foreach (var item in range)
+            {
+                this.Push(item);
+            }
+
+            return this;
+        }
+    }
+}
---
+++ Csharp OOP/Inheritance - Lab/CustomStack/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            Console.WriteLine(stackOfStrings.IsEmpty()); 
+            stackOfStrings.AddRange("2", "1", "0") ;
+            Console.WriteLine(stackOfStrings.IsEmpty());
+        }
+    }
+}
