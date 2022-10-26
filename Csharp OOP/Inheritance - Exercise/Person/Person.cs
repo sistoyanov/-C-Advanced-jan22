61046ef29dde5e2ba5f764cf521e16db34e88c21
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-26 15:54:28 +0300

    Upload

---
+++ Csharp OOP/Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Person
+{
+    public class Person
+    {
+        //private int age;
+        //private string name;
+        public Person(string name, int age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+        public virtual int Age { get; set; }
+
+        //{
+        //    get { return age; }
+        //    set
+        //    {
+        //        if (value > -1)
+        //        {
+        //            this.age = value;
+        //        }
+        //    }
+        //}
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
