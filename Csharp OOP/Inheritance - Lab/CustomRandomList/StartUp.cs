588145ceeebc19612ab6af5fc92219bb3c6c611a
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-26 10:15:37 +0300

    Upload

---
+++ Csharp OOP/Inheritance - Lab/CustomRandomList/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList strings = new RandomList { "1", "2", "3", "4"};
+            Console.WriteLine(strings.RandomString());
+        }
+    }
+}
