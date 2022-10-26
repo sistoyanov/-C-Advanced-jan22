1992210edbd6e5665c275290e765b2c7e018c191
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-26 09:57:36 +0300

    Upload

---
+++ Csharp OOP/Inheritance - Lab/Farm/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy dog = new Puppy();
+            dog.Eat();
+            dog.Bark();
+            dog.Weep();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+        }
+    }
+}
