b6bdc4bb46d84b48f928cc40aa06ecd55d9fd886
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-28 14:04:09 +0300

    Upload

---
+++ Csharp OOP/Encapsulation - Lab/02.Salary/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            decimal bonus = 0;
+
+            if (this.Age > 30)
+            {
+                bonus = (this.Salary * percentage) / 100;
+                this.Salary += bonus;
+            }
+            else
+            {
+                bonus = (this.Salary * percentage) / 200;
+                this.Salary += bonus;
+            }
+        }
+
+    }
+}
