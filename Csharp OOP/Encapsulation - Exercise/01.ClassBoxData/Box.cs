e14995df3f918e8418c5acdf285604ec4340e6d0
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-10-31 10:51:30 +0200

    Upload

---
+++ Csharp OOP/Encapsulation - Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+	{
+		private double length;
+        private double width;
+        private double height;
+
+		public Box(double length, double width, double height)
+		{
+			this.Length = length;
+			this.Width = width;
+			this.Height = height;
+		}
+
+        public double Length
+        {
+			get { return length; }
+			set 
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException("Length cannot be zero or negative.");
+				}
+
+				length = value; 
+			}
+		}
+
+		public double Width
+        {	
+			get { return width; }
+			set 
+			{
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                width = value; 
+			}
+		}
+
+		public double Height
+        {
+			get { return height; }
+			set 
+			{
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                height = value; 
+			}
+		}
+
+		public double SurfaceArea()
+		{
+			double surfaceArea = (2 * length * width) + (2 * length * height) + (2 * width * height);
+			return surfaceArea;
+		}
+
+		public double LateralSurfaceArea()
+		{
+			double lateralSurfaceArea = (2 * length * height) + (2 * width * height);
+			return lateralSurfaceArea;
+		}
+
+		public double Volume()
+		{
+			double volume = length * width * height;
+			return volume;
+		}
+    }
+}
