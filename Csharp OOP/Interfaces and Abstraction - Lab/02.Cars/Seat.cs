80e53b2d94448462413236d59916d2f9601db6cb
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-11-02 10:28:35 +0200

    Upload

---
+++ Csharp OOP/Interfaces and Abstraction - Lab/02.Cars/Seat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model { get ; set ; }
+        public string Color { get ; set ; }
+
+        public override string ToString()
+        {
+            return $"{this.Color} {GetType().Name} {this.Model}";
+        }
+    }
+}
