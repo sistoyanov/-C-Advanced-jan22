59c6b8a3a86a050d8088bde2761861a8d54f4d93
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-11-09 14:46:48 +0200

    Upload

---
+++ Csharp OOP/Exceptions and Error Handling - Lab/04.SumOfIntegers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace _04.SumOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int sum = 0;
+
+            foreach (var item in input)
+            {
+                try
+                {
+                    int currentNum = int.Parse(item);
+                    sum += currentNum;
+                }
+                catch (FormatException)
+                {
+
+                    Console.WriteLine($"The element '{item}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{item}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
