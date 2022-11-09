4f59843305334892cff1625f04fc543153d093fe
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-11-09 10:10:04 +0200

    Upload

---
+++ Csharp OOP/Exceptions and Error Handling - Lab/01.SquareRoot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(CalculateSquare(num));
+            }
+            catch (ArgumentException ae)
+            {
+
+                Console.WriteLine(ae.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        private static double CalculateSquare(int num)
+        {
+            if (num < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+            else
+            {
+                return Math.Sqrt(num);
+            }
+        }
+    }
+
+    
+}
