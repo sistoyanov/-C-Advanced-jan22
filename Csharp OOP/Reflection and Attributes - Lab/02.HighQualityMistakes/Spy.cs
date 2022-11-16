4a30a2b03a9c9c461e389c947dd2f815645d5156
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-11-16 13:12:35 +0200

    Upload

---
+++ Csharp OOP/Reflection and Attributes - Lab/02.HighQualityMistakes/Spy.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] fieldsToInvestigate)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            object classInstance = Activator.CreateInstance(classType);
+
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            StringBuilder output = new StringBuilder();
+
+            
+            output.AppendLine($"Class under investigation: {investigatedClass}");
+
+            foreach (FieldInfo field in classFields.Where(f => fieldsToInvestigate.Contains(f.Name)))
+            {
+                output.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            Type type = Type.GetType(className);
+            object instance = Activator.CreateInstance(type);
+            StringBuilder output = new StringBuilder();
+
+            FieldInfo[] typeFields = type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo[] typePublicMethods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+            MethodInfo[] tyepNonPublicMethods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (FieldInfo field in typeFields)
+            {
+                output.AppendLine($"{field.Name} must be private!");
+            }
+
+            foreach (MethodInfo method in tyepNonPublicMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                output.AppendLine($"{method.Name} have to be public!");
+            }
+
+            foreach (MethodInfo method in typePublicMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                output.AppendLine($"{method.Name} have to be private!");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
