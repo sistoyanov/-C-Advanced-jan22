3a59c5d7ac4527198b5af91a52a0ee281240a070
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-11-16 11:51:28 +0200

    Upload

---
+++ Csharp OOP/Reflection and Attributes - Lab/01.Stealer/Spy.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] fieldsToInvestigate)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            object classInstance = Activator.CreateInstance(classType);
+
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            StringBuilder output = new StringBuilder();
+
+            
+            output.AppendLine($"Class under investigation: {investigatedClass}");
+
+            foreach (FieldInfo field in classFields.Where(f => fieldsToInvestigate.Contains(f.Name)))
+            {
+                output.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+
+
+
+    }
+}
