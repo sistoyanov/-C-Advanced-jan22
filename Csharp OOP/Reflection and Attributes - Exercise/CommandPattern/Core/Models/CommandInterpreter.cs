0523768c894668e1d39a5a203715aa2f48098c2b
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-11-17 14:05:49 +0200

    Upload

---
+++ Csharp OOP/Reflection and Attributes - Exercise/CommandPattern/Core/Models/CommandInterpreter.cs
@@ -0,0 +1,25 @@
+﻿
+namespace CommandPattern.Core.Models
+{
+    using Contracts;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandDetails = args.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+            string commandType = commandDetails[0];
+            string[] commandArgs = commandDetails.Skip(1).ToArray();
+
+            Type type = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(x => x.Name == commandType + "Command");
+            ICommand currentComand = (ICommand)Activator.CreateInstance(type);
+
+            string result = currentComand.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ Csharp OOP/Reflection and Attributes - Exercise/CommandPattern/Core/Models/Engine.cs
@@ -0,0 +1,25 @@
+﻿
+namespace CommandPattern.Core.Models
+{
+    using Contracts;
+    using System;
+
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandIterpreter)
+        {
+            this.commandInterpreter = commandIterpreter;
+        }
+
+        public void Run()
+        {
+            string input = Console.ReadLine();
+
+            string result = commandInterpreter.Read(input);
+
+            Console.WriteLine(result);
+        }
+    }
+}
