6467461aaed45ce9328d49deaaad5731a4271fb4
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-11-17 09:38:11 +0200

    Upload

---
+++ Csharp OOP/Reflection and Attributes - Lab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(Program);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+            foreach (MethodInfo method in methods)
+            {
+                
+                if (method.CustomAttributes.Any(m => m.AttributeType == typeof(AuthorAttribute)))
+                {
+                    object[] attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute attribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+
+                }
+            }
+        }
+    }
+}
