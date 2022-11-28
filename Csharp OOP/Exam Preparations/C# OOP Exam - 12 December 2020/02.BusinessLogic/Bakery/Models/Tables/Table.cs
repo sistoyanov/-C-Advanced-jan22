39b30a97303f8c9f58a48d48f75ef5095719964e
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-11-28 09:37:00 +0200

    Upload

--- Csharp OOP/Exam Preparations/C# OOP Exam - 12 December 2020/02.BusinessLogic/Bakery/Models/Tables/Table.cs
+++ Csharp OOP/Exam Preparations/C# OOP Exam - 12 December 2020/02.BusinessLogic/Bakery/Models/Tables/Table.cs
@@ -1,96 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Bakery.Models.BakedFoods.Contracts;
-using Bakery.Models.Drinks.Contracts;
-using Bakery.Models.Tables.Contracts;
-using Bakery.Utilities.Messages;
-
+﻿
 namespace Bakery.Models.Tables
 {
+    using BakedFoods.Contracts;
+    using Bakery.Utilities.Messages;
+    using Drinks.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using Tables.Contracts;
     public abstract class Table : ITable
     {
+        private ICollection<IBakedFood> foodOrders;
+        private ICollection<IDrink> drinkOrders;
         private int capacity;
         private int numberOfPeople;
-        private readonly ICollection<IBakedFood> foodOrders;
-        private readonly ICollection<IDrink> drinkOrders;
 
         public Table(int tableNumber, int capacity, decimal pricePerPerson)
         {
+            this.foodOrders = new List<IBakedFood>();
+            this.drinkOrders = new List<IDrink>();
             this.TableNumber = tableNumber;
             this.Capacity = capacity;
             this.PricePerPerson = pricePerPerson;
             this.IsReserved = false;
-            this.foodOrders = new List<IBakedFood>();
-            this.drinkOrders = new List<IDrink>();
         }
 
-        public int TableNumber { get; }
+        public int TableNumber  { get; private set; } 
 
         public int Capacity
         {
-            get => this.capacity;
-            private set
+            get { return this.capacity; }
+            private set 
             {
                 if (value < 0)
                 {
-                    throw new ArgumentException(ExceptionMessages.InvalidTableCapacity);
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidTableCapacity));
                 }
 
-                this.capacity = value;
+                this.capacity = value; 
             }
         }
 
-        public int NumberOfPeople
+        public int NumberOfPeople 
         {
-            get => this.numberOfPeople;
-            private set
+            get { return this.numberOfPeople; }
+            private set                                          
             {
-                if (value < 0)
+                if (value <= 0)
                 {
                     throw new ArgumentException(ExceptionMessages.InvalidNumberOfPeople);
                 }
 
-                this.numberOfPeople = value;
-            }
+                this.numberOfPeople = value; 
+            }                                                   
         }
 
-        public decimal PricePerPerson { get; }
+        public decimal PricePerPerson { get; private set; }
 
         public bool IsReserved { get; private set; }
 
-        public decimal Price => this.NumberOfPeople * this.PricePerPerson;
+        public decimal Price => this.PricePerPerson * this.NumberOfPeople;
 
         public void Clear()
         {
-            this.drinkOrders.Clear();
             this.foodOrders.Clear();
-            this.NumberOfPeople = 0;
+            this.drinkOrders.Clear();
             this.IsReserved = false;
+            this.numberOfPeople = 0; 
         }
 
-        public decimal GetBill()
+        public decimal GetBill() 
         {
-            return this.Price + this.foodOrders.Sum(f => f.Price) + this.drinkOrders.Sum(d => d.Price);
+            decimal totalFoodsPrice = this.foodOrders.Sum(f => f.Price);
+            decimal totalDrinksPRice = this.drinkOrders.Sum(d => d.Price);
+
+            return this.Price + totalFoodsPrice + totalDrinksPRice;
         }
 
         public string GetFreeTableInfo()
         {
-            var sb = new StringBuilder();
+            StringBuilder output = new StringBuilder();
+            output.AppendLine($"Table: {this.TableNumber}")
+                  .AppendLine($"Type: {this.GetType().Name}")        
+                  .AppendLine($"Capacity: {this.Capacity}")
+                  .AppendLine($"Price per Person: {this.PricePerPerson}");
 
-            sb.AppendLine($"Table: {this.TableNumber}");
-            sb.AppendLine($"Type: {this.GetType().Name}");
-            sb.AppendLine($"Capacity: {this.Capacity}");
-            sb.AppendLine($"Price per Person: {this.PricePerPerson}");
-
-            return sb.ToString().TrimEnd();
+            return output.ToString().TrimEnd();
         }
 
         public void OrderDrink(IDrink drink)
         {
-            this.drinkOrders.Add(drink);
+           this.drinkOrders.Add(drink);
         }
 
         public void OrderFood(IBakedFood food)
