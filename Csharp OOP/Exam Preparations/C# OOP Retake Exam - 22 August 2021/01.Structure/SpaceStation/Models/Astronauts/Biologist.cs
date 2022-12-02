2252803c4a226e9be8532c55034a9ee8d9c6a27d
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-12-02 16:44:10 +0200

    Upload 01.Structure

---
+++ Csharp OOP/Exam Preparations/C# OOP Retake Exam - 22 August 2021/01.Structure/SpaceStation/Models/Astronauts/Biologist.cs
@@ -0,0 +1,20 @@
+﻿namespace SpaceStation.Models.Astronauts
+{
+    public class Biologist : Astronaut
+    {
+        private const double BiologistOxygen = 70;
+        public Biologist(string name) : base(name, BiologistOxygen)
+        {
+        }
+
+        public override void Breath()
+        {
+            this.Oxygen -= 5;
+
+            if (this.Oxygen < 0)
+            {
+                this.Oxygen = 0;
+            }
+        }
+    }
+}
---
+++ Csharp OOP/Exam Preparations/C# OOP Retake Exam - 22 August 2021/01.Structure/SpaceStation/Models/Mission/Mission.cs
@@ -0,0 +1,34 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Mission.Contracts;
+using SpaceStation.Models.Planets.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpaceStation.Models.Mission
+{
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+
+            foreach (IAstronaut astronaut in astronauts)
+            {
+               while (planet.Items.Count > 0 && astronaut.Oxygen > 0)
+               {
+                   string currentItem = planet.Items.FirstOrDefault();
+                   astronaut.Breath();
+                   astronaut.Bag.Items.Add(currentItem);
+                   planet.Items.Remove(currentItem);
+
+               }
+
+                if (planet.Items.Count == 0)
+                {
+                    break;
+                }
+
+            }
+            
+        }
+    }
+}
