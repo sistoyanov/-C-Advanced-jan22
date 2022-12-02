8f12aea6b6ac4d679c6a56c0295ba7f9e7acdeed
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-12-02 11:14:10 +0200

    Upload 02. Business Logic  Skeleton

---
+++ Csharp OOP/Exam Preparations/C# OOP Exam - 15 August 2021/02.BusinessLogic/CarRacing/Models/Cars/SuperCar.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRacing.Models.Cars
+{
+    public class SuperCar : Car
+    {
+        private const double SuperCarFuelAvailable = 80;
+        private const double SuperCarFuelConsumtpionPerRace  = 10;
+        public SuperCar(string make, string model, string vin, int horsePower) : base(make, model, vin, horsePower, SuperCarFuelAvailable, SuperCarFuelConsumtpionPerRace)
+        {
+        }
+    }
+}
---
+++ Csharp OOP/Exam Preparations/C# OOP Exam - 15 August 2021/02.BusinessLogic/CarRacing/Models/Cars/TunedCar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CarRacing.Models.Cars
+{
+    public class TunedCar : Car
+    {
+        private const double TunedCarFuelAvailable = 65;
+        private const double TunedCarFuelConsumtpionPerRace = 7.5;
+        public TunedCar(string make, string model, string vin, int horsePower) : base(make, model, vin, horsePower, TunedCarFuelAvailable, TunedCarFuelConsumtpionPerRace)
+        {
+        }
+
+        public override void Drive()   /// validate if this works
+        {
+            base.Drive();
+            this.HorsePower = (int)Math.Round(this.HorsePower * 0.97);
+        }
+    }
+}
