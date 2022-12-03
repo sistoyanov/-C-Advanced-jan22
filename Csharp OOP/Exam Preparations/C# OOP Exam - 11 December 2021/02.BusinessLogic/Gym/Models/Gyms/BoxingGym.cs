9b73df350a6963a01c9ccb4ae204389739bada53
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-12-03 19:00:22 +0200

    Upload 02. Business Logic Skeleton

---
+++ Csharp OOP/Exam Preparations/C# OOP Exam - 11 December 2021/02.BusinessLogic/Gym/Models/Gyms/BoxingGym.cs
@@ -0,0 +1,10 @@
+﻿namespace Gym.Models.Gyms
+{
+    public class BoxingGym : Gym
+    {
+        private const int BoxingGymCapacity = 15;
+        public BoxingGym(string name) : base(name, BoxingGymCapacity)
+        {
+        }
+    }
+}
