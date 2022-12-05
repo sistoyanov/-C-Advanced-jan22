cafa9b2af3663765a57203d24ed47cfbe44db100
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-12-05 11:44:10 +0200

    Upload 02. Business Logic Skeleton

---
+++ Csharp OOP/Exam Preparations/C# OOP Exam - 09 April 2022/02.BusinessLogic/Formula1/Repositories/FormulaOneCarRepository.cs
@@ -0,0 +1,26 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Formula1.Repositories
+{
+    public class FormulaOneCarRepository : IRepository<IFormulaOneCar>
+    {
+        private readonly ICollection<IFormulaOneCar> models;
+
+        public FormulaOneCarRepository()
+        {
+            this.models = new List<IFormulaOneCar>();
+        }
+
+        public IReadOnlyCollection<IFormulaOneCar> Models => this.models as IReadOnlyCollection<IFormulaOneCar>;
+
+        public void Add(IFormulaOneCar model) => this.models.Add(model);
+
+        public bool Remove(IFormulaOneCar model) => models.Remove(model);
+
+        public IFormulaOneCar FindByName(string name) => this.models.FirstOrDefault(m => m.Model == name);
+
+    }
+}
---
+++ Csharp OOP/Exam Preparations/C# OOP Exam - 09 April 2022/02.BusinessLogic/Formula1/Repositories/PilotRepository.cs
@@ -0,0 +1,25 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Formula1.Repositories
+{
+    public class PilotRepository : IRepository<IPilot>
+    {
+        private readonly ICollection<IPilot> models;
+
+        public PilotRepository()
+        {
+            this.models = new List<IPilot>();
+        }
+
+        public IReadOnlyCollection<IPilot> Models => this.models as IReadOnlyCollection<IPilot>;
+
+        public void Add(IPilot model) => this.models.Add(model);
+
+        public bool Remove(IPilot model) => models.Remove(model);
+
+        public IPilot FindByName(string name) => this.models.FirstOrDefault(m => m.FullName == name);
+    }
+}
