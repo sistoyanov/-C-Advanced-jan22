8c339efa7660e8c7f2aac44cdecfe6f630be259a
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-12-05 11:42:49 +0200

    Upload 01. Structure

---
+++ Csharp OOP/Exam Preparations/C# OOP Exam - 09 April 2022/01.Structure/Formula1/Models/Pilot.cs
@@ -0,0 +1,72 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Utilities;
+using System;
+
+namespace Formula1.Models
+{
+    public class Pilot : IPilot
+    {
+        private string fullName;
+        //bool canRace;
+        private IFormulaOneCar car;
+
+        public Pilot(string fullName)
+        {
+            this.FullName = fullName;
+        }
+
+        public Pilot()
+        {
+            //this.canRace = false;
+        }
+
+        public string FullName
+        {
+            get { return this.fullName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidPilot, value));
+                }
+
+                this.fullName = value;
+            }
+        }
+        
+        public bool CanRace { get; private set; }//=> this.canRace;
+
+        public IFormulaOneCar Car
+        {
+            get { return this.car; }
+            private set 
+            {
+                if (value is null)
+                {
+                    throw new NullReferenceException(ExceptionMessages.InvalidCarForPilot);
+                }
+
+                this.car = value; 
+            }
+        }
+
+        public int NumberOfWins { get; private set; }
+
+
+        public void AddCar(IFormulaOneCar car)
+        {
+            this.Car = car;
+            this.CanRace = true;
+        }
+
+        public void WinRace()
+        {
+            this.NumberOfWins += 1;
+        }
+
+        public override string ToString()
+        {
+            return $"Pilot {this.FullName} has {this.NumberOfWins} wins.";
+        }
+    }
+}
---
+++ Csharp OOP/Exam Preparations/C# OOP Exam - 09 April 2022/01.Structure/Formula1/Repositories/RaceRepository.cs
@@ -0,0 +1,25 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Formula1.Repositories
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        private readonly ICollection<IRace> models;
+
+        public RaceRepository()
+        {
+            this.models = new List<IRace>();
+        }
+
+        public IReadOnlyCollection<IRace> Models => this.models as IReadOnlyCollection<IRace>;
+
+        public void Add(IRace model) => this.models.Add(model);
+
+        public bool Remove(IRace model) => models.Remove(model);
+
+        public IRace FindByName(string name) => this.models.FirstOrDefault(m => m.RaceName == name);
+    }
+}
