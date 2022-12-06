190eb593d954b054522c7c0a1930595255b94547
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-12-06 14:17:53 +0200

    Upload 02. Business Logic Skeleton

---
+++ Csharp OOP/Exam Preparations/C# OOP Retake Exam - 18 April 2022/02.BusinessLogic/Heroes/Models/Map/Map.cs
@@ -0,0 +1,74 @@
+﻿using Heroes.Models.Contracts;
+using Heroes.Models.Heroes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Heroes.Models.Map
+{
+    public class Map : IMap
+    {
+        public string Fight(ICollection<IHero> players)
+        {
+            ICollection<Barbarian> barbarians = new List<Barbarian>();
+            ICollection<Knight> knights = new List<Knight>();
+            string output = string.Empty;
+
+            foreach (IHero hero in players)
+            {
+                if (hero.GetType().Name == "Barbarian")
+                {
+                    barbarians.Add(hero as Barbarian);
+                }
+                else if (hero.GetType().Name == "Knight")
+                {
+                    knights.Add(hero as Knight);
+                }
+            }
+
+            while (knights.Any(k => k.IsAlive) && barbarians.Any(b => b.IsAlive))
+            {
+                foreach (var knight in knights)
+                {
+                    if (knight.IsAlive)
+                    {
+
+                        foreach (var barberian in barbarians)
+                        {
+                            barberian.TakeDamage(knight.Weapon.DoDamage());
+                        }
+
+                    }
+
+                }
+
+
+                foreach (var barberian in barbarians)
+                {
+                    if (barberian.IsAlive)
+                    {
+
+                        foreach (var knight in knights)
+                        {
+                            knight.TakeDamage(barberian.Weapon.DoDamage());
+                        }
+
+                    }
+
+                }
+
+            }
+
+
+            if (knights.Count(k => k.IsAlive) > 0)
+            {
+                output = $"The knights took {knights.Count(k => !k.IsAlive)} casualties but won the battle.";
+            }
+            else if (barbarians.Count(b => b.IsAlive) > 0)
+            {
+                output = $"The barbarians took {barbarians.Count(b => !b.IsAlive)} casualties but won the battle.";
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Csharp OOP/Exam Preparations/C# OOP Retake Exam - 18 April 2022/02.BusinessLogic/Heroes/Repositories/HeroRepository.cs
@@ -0,0 +1,27 @@
+﻿
+using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Heroes.Repositories
+{
+    public class HeroRepository : IRepository<IHero>
+    {
+        private readonly ICollection<IHero> models;
+
+        public HeroRepository()
+        {
+            this.models = new List<IHero>();
+        }
+
+        public IReadOnlyCollection<IHero> Models => this.models as IReadOnlyCollection<IHero>;
+
+        public void Add(IHero model) => this.models.Add(model);
+
+        public bool Remove(IHero model) => this.models.Remove(model);
+
+        public IHero FindByName(string name) => this.models.FirstOrDefault(m => m.Name == name);
+
+    }
+}
